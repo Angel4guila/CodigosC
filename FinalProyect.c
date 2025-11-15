c982fe0563d4c4c2969a90167ac3ce234a1b8226
Angel4guila <163228472+Angel4guila@users.noreply.github.com>
2025-11-14 20:13:06 -0600

    Initialize sorting program with menu options

---
+++ FinalProyect.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//void Burbuja()
+//void Combsort()
+//void QuickSort()
+//void ShellSort
+
+int main(){
+
+  do{
+  
+  int op=0;
+  
+  printf("==========Programa de ordenamiento==========");
+  printf("1. Generar lista de numeros aleatorios"); //en la funcion preguntar cuentos elementos se van a generar, el nombre que va a tener el archivo
+  printf("2. Cargar la lista con la que se va a trabajar"); //opcional, pues al selecionar algun metodo, preguntar con que lista se va a ordenar
+  printf("3. Burbuja");
+  printf("4. Combsort");
+  printf("5. Quick sort");
+  printf("6. Shell sort");
+  printf("7. Mostrar lista ordenada");
+  printf("8. Salir")
+  
+    switch(op){
+      case 1:
+      
+      break;
+      
+      case 2:
+      
+      break;
+      
+      case 3:
+      
+      break;
+      
+      case 4:
+      
+      break;
+      
+      case 5:
+      
+      break;
+      
+      case 6:
+      
+      break;
+      
+      case 7:
+      
+      break;
+      case 8:
+      
+      break;
+      default:
+      
+        printf("Opcion Invalida, escoger otra opcion");
+      
+      break;
+    
+    
+    
+    }
+  
+  
+  }while(op != 0);
+
+
+
+
+return 0;
+}
